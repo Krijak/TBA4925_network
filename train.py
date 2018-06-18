ae3df4b872a0031f584ce87992bb0de24b869353
Kristina Jakobsen <krijak@stud.ntnu.no>
2018-06-18 09:39:30 +0200

    multi gpu?

--- train.py
+++ train.py
@@ -135,6 +135,11 @@ def create_model(input_shape, anchors, num_classes, load_pretrained=True, freeze
         arguments={'anchors': anchors, 'num_classes': num_classes, 'ignore_thresh': 0.5})(
         [*model_body.output, *y_true])
     model = Model([model_body.input, *y_true], model_loss)
+
+    gpus = get_number_of_gpus()
+    print('Found {} gpus'.format(gpus))
+    if gpus > 1:
+        model = ModelMGPU(model, gpus)
     #print('Training with two gpus')
     #with tf.device("/cpu:0"):
     #   model = ...
