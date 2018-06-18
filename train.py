b8d735bba3e4bd24655ff85fe6d01aa2fc67bb23
Kristina Jakobsen <krijak@stud.ntnu.no>
2018-06-18 13:03:16 +0200

    tried make_parallel

--- train.py
+++ train.py
@@ -60,7 +60,8 @@ def _main():
         gpus = get_number_of_gpus()
         print('Found {} gpus'.format(gpus))
         if gpus > 1:
-            model = ModelMGPU(model, gpus)
+            model = make_parallel(model, [0,1])
+            #model = ModelMGPU(model, gpus)
 
         #model.compile(optimizer=Adam(lr=1e-3), loss={
             # use custom yolo_loss Lambda layer.
@@ -145,11 +146,6 @@ def create_model(input_shape, anchors, num_classes, load_pretrained=True, freeze
         arguments={'anchors': anchors, 'num_classes': num_classes, 'ignore_thresh': 0.5})(
         [*model_body.output, *y_true])
     model = Model([model_body.input, *y_true], model_loss)
-    #model = Model([model_body.input, *y_true], )
-    #print('Training with two gpus')
-    #with tf.device("/cpu:0"):
-    #   model = ...
-    #model = multi_gpu_model(model, gpus=2)
     return model
 
 def create_tiny_model(input_shape, anchors, num_classes, load_pretrained=True, freeze_body=2,
@@ -181,6 +177,48 @@ def create_tiny_model(input_shape, anchors, num_classes, load_pretrained=True, f
     model = Model([model_body.input, *y_true], model_loss)
 
     return model
+
+def make_parallel(model, gpu_list):
+    def get_slice(data, idx, parts):
+        shape = tf.shape(data)
+        size = tf.concat([ shape[:1] // parts, shape[1:] ], axis=0)
+        stride = tf.concat([ shape[:1] // parts, shape[1:]*0 ], axis=0)
+        start = stride * idx
+        return tf.slice(data, start, size)
+
+    outputs_all = []
+    for i in range(len(model.outputs)):
+        outputs_all.append([])
+
+    #Place a copy of the model on each GPU, each getting a slice of the batch
+    gpu_count = len(gpu_list)
+    for i in range(gpu_count):
+        with tf.device('/gpu:%d' % gpu_list[i]):
+            with tf.name_scope('tower_%d' % gpu_list[i]) as scope:
+
+                inputs = []
+                #Slice each input into a piece for processing on this GPU
+                for x in model.inputs:
+                    input_shape = tuple(x.get_shape().as_list())[1:]
+                    slice_n = Lambda(get_slice, output_shape=input_shape, arguments={'idx':i,'parts':gpu_count})(x)
+                    inputs.append(slice_n)                
+
+                outputs = model(inputs)
+                
+                if not isinstance(outputs, list):
+                    outputs = [outputs]
+                
+                #Save all the outputs for merging back together later
+                for l in range(len(outputs)):
+                    outputs_all[l].append(outputs[l])
+
+    # merge outputs on CPU
+    with tf.device('/cpu:0'):
+        merged = []
+        for outputs in outputs_all:
+            merged.append(concatenate(outputs, axis=0))
+            
+        return Model(inputs=model.inputs, outputs=merged)
 
 def data_generator(annotation_lines, batch_size, input_shape, anchors, num_classes):
     '''data generator for fit_generator'''
