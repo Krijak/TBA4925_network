2f969547bda976f40970a7db4b8928a9f4a3cc5e
Kristina Jakobsen <krijak@stud.ntnu.no>
2018-06-18 12:39:40 +0200

    multi gpu?

--- train.py
+++ train.py
@@ -144,8 +144,8 @@ def create_model(input_shape, anchors, num_classes, load_pretrained=True, freeze
     model_loss = Lambda(yolo_loss, output_shape=(1,), name='yolo_loss',
         arguments={'anchors': anchors, 'num_classes': num_classes, 'ignore_thresh': 0.5})(
         [*model_body.output, *y_true])
-    #model = Model([model_body.input, *y_true], model_loss)
-    model = Model([model_body.input, *y_true], )
+    model = Model([model_body.input, *y_true], model_loss)
+    #model = Model([model_body.input, *y_true], )
     #print('Training with two gpus')
     #with tf.device("/cpu:0"):
     #   model = ...
