e3575bdf43451ab1fecd0a508b5495dc9948c5a4
Kristina Jakobsen <krijak@stud.ntnu.no>
2018-06-19 09:35:13 +0200

    test train annotation file

--- train.py
+++ train.py
@@ -70,7 +70,7 @@ def _main():
 
 
         #model.compile(optimizer=Adam(lr=1e-3), loss=categorical_crossentropy(y_true, y_pred))
-        batch_size = 32
+        batch_size = 16
         print('Train on {} samples, val on {} samples, with batch size {}.'.format(num_train, num_val, batch_size))
         model.fit_generator(data_generator_wrapper(lines[:num_train], batch_size, input_shape, anchors, num_classes),
                 steps_per_epoch=max(1, num_train//batch_size),
